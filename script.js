2d7ac7adba76a43ec7eda5d3297995299d26e1da
shringiaashita-beep <shringiaashita@gmail.com>
2026-02-27 21:10:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 400;
-canvas.height = 600;
+canvas.width = 1300;
+canvas.height = 1700;
 
 
 // ==============================
